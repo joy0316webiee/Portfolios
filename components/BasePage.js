9376067c97fe8e772f7c99ebb45c5424f70c3395
joy0316webiee <joy0316webiee@gmail.com>
2019-10-16 16:43:28 +0200

    Completed page

--- components/BasePage.js
+++ components/BasePage.js
@@ -1,17 +1,24 @@
 import { Container } from 'reactstrap';
+import PropTypes from 'prop-types';
 
-const BasePage = props => {
-  const { className } = props;
+const BasePage = (props) => {
+  const { className, title, containerClass } = props;
+
+  // const className = props.className || '';
 
   return (
     <div className={`base-page ${className}`}>
-      <Container>{props.children}</Container>
+      <Container className={containerClass}>
+        { title && <div className="page-header"><h1 className="page-header-title">{title}</h1></div>}
+        {props.children}
+      </Container>
     </div>
-  );
-};
+  )
+}
 
 BasePage.defaultProps = {
-  className: ''
-};
+  className: '',
+  containerClass: ''
+}
 
 export default BasePage;
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,39 +1,38 @@
 import React from 'react';
-import { withRouter } from 'next/router';
-import axios from 'axios';
-
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
+import { withRouter } from 'next/router'
+import axios from 'axios';
 
 class Portfolio extends React.Component {
-  static async getInitialProps({ query }) {
-    let portfolio = {};
+
+  static async getInitialProps({query}) {
     const portfolioId = query.id;
+    let portfolio = {};
 
     try {
-      const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/posts/${portfolioId}`
-      );
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${portfolioId}`);
       portfolio = response.data;
-    } catch (err) {
+
+    } catch(err) {
       console.error(err);
     }
 
-    return { portfolio };
+    return {portfolio};
   }
 
   render() {
     const { portfolio } = this.props;
 
     return (
-      <BaseLayout>
+      <BaseLayout {...this.props.auth}>
         <BasePage>
-          <h1>I am Portfolio Page</h1>
-          <p>TITLE: {portfolio.title}</p>
-          <p>BODY: {portfolio.body}</p>
+          <h1> {portfolio.title} </h1>
+          <p> BODY: {portfolio.body} </p>
+          <p> ID:  {portfolio.id} </p>
         </BasePage>
       </BaseLayout>
-    );
+    )
   }
 }
 
